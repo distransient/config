b6e5b4d64ff107452a879921cacaae62cec12001
Kel <humanoidanalog@gmail.com>
2014-12-10 22:35:41 -0800

    add highlighter definitions

--- lib/syntax-highlighting.zsh
+++ lib/syntax-highlighting.zsh
@@ -27,6 +27,7 @@
 # -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 # vim: ft=zsh sw=2 ts=2 et
 # -------------------------------------------------------------------------------------------------
+# Source: https://github.com/zsh-users/zsh-syntax-highlighting
 
 
 # -------------------------------------------------------------------------------------------------
@@ -202,12 +203,393 @@ _zsh_highlight_bind_widgets || {
   return 1
 }
 
-# Resolve highlighters directory location.
-_zsh_highlight_load_highlighters "${ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR:-${${0:A}:h}/highlighters}" || {
-  echo 'zsh-syntax-highlighting: failed loading highlighters, exiting.' >&2
+# Reset scratch variables when commandline is done.
+_zsh_highlight_preexec_hook()
+{
+  _ZSH_HIGHLIGHT_PRIOR_BUFFER=
+  _ZSH_HIGHLIGHT_PRIOR_CURSOR=
+}
+autoload -U add-zsh-hook
+add-zsh-hook preexec _zsh_highlight_preexec_hook 2>/dev/null || {
+    echo 'zsh-syntax-highlighting: failed loading add-zsh-hook.' >&2
+  }
+
+# Initialize the array of active highlighters if needed.
+[[ $#ZSH_HIGHLIGHT_HIGHLIGHTERS -eq 0 ]] && ZSH_HIGHLIGHT_HIGHLIGHTERS=(main) || true
+
+## brackets-highlighter.zsh
+# Define default styles.
+: ${ZSH_HIGHLIGHT_STYLES[bracket-error]:=fg=red,bold}
+: ${ZSH_HIGHLIGHT_STYLES[bracket-level-1]:=fg=blue,bold}
+: ${ZSH_HIGHLIGHT_STYLES[bracket-level-2]:=fg=green,bold}
+: ${ZSH_HIGHLIGHT_STYLES[bracket-level-3]:=fg=magenta,bold}
+: ${ZSH_HIGHLIGHT_STYLES[bracket-level-4]:=fg=yellow,bold}
+: ${ZSH_HIGHLIGHT_STYLES[bracket-level-5]:=fg=cyan,bold}
+: ${ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]:=standout}
+
+# Whether the brackets highlighter should be called or not.
+_zsh_highlight_brackets_highlighter_predicate()
+{
+  _zsh_highlight_cursor_moved || _zsh_highlight_buffer_modified
+}
+
+# Brackets highlighting function.
+_zsh_highlight_brackets_highlighter()
+{
+  local level=0 pos
+  local -A levelpos lastoflevel matching typepos
+  region_highlight=()
+
+  # Find all brackets and remember which one is matching
+  for (( pos = 0; $pos < ${#BUFFER}; pos++ )) ; do
+    local char="$BUFFER[pos+1]"
+    case $char in
+      ["([{"])
+        levelpos[$pos]=$((++level))
+        lastoflevel[$level]=$pos
+        _zsh_highlight_brackets_highlighter_brackettype "$char"
+        ;;
+      [")]}"])
+        matching[$lastoflevel[$level]]=$pos
+        matching[$pos]=$lastoflevel[$level]
+        levelpos[$pos]=$((level--))
+        _zsh_highlight_brackets_highlighter_brackettype "$char"
+        ;;
+      ['"'\'])
+        # Skip everything inside quotes
+        local quotetype=$char
+        while (( $pos < ${#BUFFER} )) ; do
+          (( pos++ ))
+          [[ $BUFFER[$pos+1] == $quotetype ]] && break
+        done
+        ;;
+    esac
+  done
+
+  # Now highlight all found brackets
+  for pos in ${(k)levelpos}; do
+    if [[ -n $matching[$pos] ]] && [[ $typepos[$pos] == $typepos[$matching[$pos]] ]]; then
+      local bracket_color_size=${#ZSH_HIGHLIGHT_STYLES[(I)bracket-level-*]}
+      local bracket_color_level=bracket-level-$(( (levelpos[$pos] - 1) % bracket_color_size + 1 ))
+      local style=$ZSH_HIGHLIGHT_STYLES[$bracket_color_level]
+      region_highlight+=("$pos $((pos + 1)) $style")
+    else
+      local style=$ZSH_HIGHLIGHT_STYLES[bracket-error]
+      region_highlight+=("$pos $((pos + 1)) $style")
+    fi
+  done
+
+  # If cursor is on a bracket, then highlight corresponding bracket, if any
+  pos=$CURSOR
+  if [[ -n $levelpos[$pos] ]] && [[ -n $matching[$pos] ]]; then
+    local otherpos=$matching[$pos]
+    local style=$ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]
+    region_highlight+=("$otherpos $((otherpos + 1)) $style")
+  fi
+}
+
+# Helper function to differentiate type 
+_zsh_highlight_brackets_highlighter_brackettype()
+{
+  case $1 in
+    ["()"]) typepos[$pos]=round;;
+    ["[]"]) typepos[$pos]=bracket;;
+    ["{}"]) typepos[$pos]=curly;;
+    *) ;;
+  esac
+}
+
+## cursor-highlighter.zsh
+# Define default styles.
+: ${ZSH_HIGHLIGHT_STYLES[cursor]:=standout}
+
+# Whether the cursor highlighter should be called or not.
+_zsh_highlight_cursor_highlighter_predicate()
+{
+  _zsh_highlight_cursor_moved
+}
+
+# Cursor highlighting function.
+_zsh_highlight_cursor_highlighter()
+{
+  region_highlight+=("$CURSOR $(( $CURSOR + 1 )) $ZSH_HIGHLIGHT_STYLES[cursor]")
+}
+
+## line-highlighter.zsh
+# Define default styles.
+: ${ZSH_HIGHLIGHT_STYLES[line]:=}
+
+# Whether the root highlighter should be called or not.
+_zsh_highlight_line_highlighter_predicate()
+{
+  _zsh_highlight_buffer_modified
+}
+
+# root highlighting function.
+_zsh_highlight_line_highlighter()
+{
+  region_highlight+=("0 $#BUFFER $ZSH_HIGHLIGHT_STYLES[line]")
+}
+
+## main-highlighter.zsh
+# Define default styles.
+: ${ZSH_HIGHLIGHT_STYLES[default]:=none}
+: ${ZSH_HIGHLIGHT_STYLES[unknown-token]:=fg=red,bold}
+: ${ZSH_HIGHLIGHT_STYLES[reserved-word]:=fg=yellow}
+: ${ZSH_HIGHLIGHT_STYLES[alias]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[builtin]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[function]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[command]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[precommand]:=fg=green,underline}
+: ${ZSH_HIGHLIGHT_STYLES[commandseparator]:=none}
+: ${ZSH_HIGHLIGHT_STYLES[hashed-command]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[path]:=underline}
+: ${ZSH_HIGHLIGHT_STYLES[path_prefix]:=underline}
+: ${ZSH_HIGHLIGHT_STYLES[path_approx]:=fg=yellow,underline}
+: ${ZSH_HIGHLIGHT_STYLES[globbing]:=fg=blue}
+: ${ZSH_HIGHLIGHT_STYLES[history-expansion]:=fg=blue}
+: ${ZSH_HIGHLIGHT_STYLES[single-hyphen-option]:=none}
+: ${ZSH_HIGHLIGHT_STYLES[double-hyphen-option]:=none}
+: ${ZSH_HIGHLIGHT_STYLES[back-quoted-argument]:=none}
+: ${ZSH_HIGHLIGHT_STYLES[single-quoted-argument]:=fg=yellow}
+: ${ZSH_HIGHLIGHT_STYLES[double-quoted-argument]:=fg=yellow}
+: ${ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]:=fg=cyan}
+: ${ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]:=fg=cyan}
+: ${ZSH_HIGHLIGHT_STYLES[assign]:=none}
+
+# Whether the highlighter should be called or not.
+_zsh_highlight_main_highlighter_predicate()
+{
+  _zsh_highlight_buffer_modified
+}
+
+# Main syntax highlighting function.
+_zsh_highlight_main_highlighter()
+{
+  emulate -L zsh
+  setopt localoptions extendedglob bareglobqual
+  local start_pos=0 end_pos highlight_glob=true new_expression=true arg style sudo=false sudo_arg=false
+  typeset -a ZSH_HIGHLIGHT_TOKENS_COMMANDSEPARATOR
+  typeset -a ZSH_HIGHLIGHT_TOKENS_PRECOMMANDS
+  typeset -a ZSH_HIGHLIGHT_TOKENS_FOLLOWED_BY_COMMANDS
+  region_highlight=()
+
+  ZSH_HIGHLIGHT_TOKENS_COMMANDSEPARATOR=(
+    '|' '||' ';' '&' '&&'
+  )
+  ZSH_HIGHLIGHT_TOKENS_PRECOMMANDS=(
+    'builtin' 'command' 'exec' 'nocorrect' 'noglob'
+  )
+  # Tokens that are always immediately followed by a command.
+  ZSH_HIGHLIGHT_TOKENS_FOLLOWED_BY_COMMANDS=(
+    $ZSH_HIGHLIGHT_TOKENS_COMMANDSEPARATOR $ZSH_HIGHLIGHT_TOKENS_PRECOMMANDS
+  )
+
+  for arg in ${(z)BUFFER}; do
+    local substr_color=0
+    local style_override=""
+    [[ $start_pos -eq 0 && $arg = 'noglob' ]] && highlight_glob=false
+    ((start_pos+=${#BUFFER[$start_pos+1,-1]}-${#${BUFFER[$start_pos+1,-1]##[[:space:]]#}}))
+    ((end_pos=$start_pos+${#arg}))
+    # Parse the sudo command line
+    if $sudo; then
+      case "$arg" in
+        # Flag that requires an argument
+        '-'[Cgprtu]) sudo_arg=true;;
+        # This prevents misbehavior with sudo -u -otherargument
+        '-'*)        sudo_arg=false;;
+        *)           if $sudo_arg; then
+                       sudo_arg=false
+                     else
+                       sudo=false
+                       new_expression=true
+                     fi
+                     ;;
+      esac
+    fi
+    if $new_expression; then
+      new_expression=false
+     if [[ -n ${(M)ZSH_HIGHLIGHT_TOKENS_PRECOMMANDS:#"$arg"} ]]; then
+      style=$ZSH_HIGHLIGHT_STYLES[precommand]
+     elif [[ "$arg" = "sudo" ]]; then
+      style=$ZSH_HIGHLIGHT_STYLES[precommand]
+      sudo=true
+     else
+      res=$(LC_ALL=C builtin type -w $arg 2>/dev/null)
+      case $res in
+        *': reserved')  style=$ZSH_HIGHLIGHT_STYLES[reserved-word];;
+        *': alias')     style=$ZSH_HIGHLIGHT_STYLES[alias]
+                        local aliased_command="${"$(alias -- $arg)"#*=}"
+                        [[ -n ${(M)ZSH_HIGHLIGHT_TOKENS_FOLLOWED_BY_COMMANDS:#"$aliased_command"} && -z ${(M)ZSH_HIGHLIGHT_TOKENS_FOLLOWED_BY_COMMANDS:#"$arg"} ]] && ZSH_HIGHLIGHT_TOKENS_FOLLOWED_BY_COMMANDS+=($arg)
+                        ;;
+        *': builtin')   style=$ZSH_HIGHLIGHT_STYLES[builtin];;
+        *': function')  style=$ZSH_HIGHLIGHT_STYLES[function];;
+        *': command')   style=$ZSH_HIGHLIGHT_STYLES[command];;
+        *': hashed')    style=$ZSH_HIGHLIGHT_STYLES[hashed-command];;
+        *)              if _zsh_highlight_main_highlighter_check_assign; then
+                          style=$ZSH_HIGHLIGHT_STYLES[assign]
+                          new_expression=true
+                        elif _zsh_highlight_main_highlighter_check_path; then
+                          style=$ZSH_HIGHLIGHT_STYLES[path]
+                        elif [[ $arg[0,1] == $histchars[0,1] || $arg[0,1] == $histchars[2,2] ]]; then
+                          style=$ZSH_HIGHLIGHT_STYLES[history-expansion]
+                        else
+                          style=$ZSH_HIGHLIGHT_STYLES[unknown-token]
+                        fi
+                        ;;
+      esac
+     fi
+    else
+      case $arg in
+        '--'*)   style=$ZSH_HIGHLIGHT_STYLES[double-hyphen-option];;
+        '-'*)    style=$ZSH_HIGHLIGHT_STYLES[single-hyphen-option];;
+        "'"*"'") style=$ZSH_HIGHLIGHT_STYLES[single-quoted-argument];;
+        '"'*'"') style=$ZSH_HIGHLIGHT_STYLES[double-quoted-argument]
+                 region_highlight+=("$start_pos $end_pos $style")
+                 _zsh_highlight_main_highlighter_highlight_string
+                 substr_color=1
+                 ;;
+        '`'*'`') style=$ZSH_HIGHLIGHT_STYLES[back-quoted-argument];;
+        *"*"*)   $highlight_glob && style=$ZSH_HIGHLIGHT_STYLES[globbing] || style=$ZSH_HIGHLIGHT_STYLES[default];;
+        *)       if _zsh_highlight_main_highlighter_check_path; then
+                   style=$ZSH_HIGHLIGHT_STYLES[path]
+                 elif [[ $arg[0,1] = $histchars[0,1] ]]; then
+                   style=$ZSH_HIGHLIGHT_STYLES[history-expansion]
+                 elif [[ -n ${(M)ZSH_HIGHLIGHT_TOKENS_COMMANDSEPARATOR:#"$arg"} ]]; then
+                   style=$ZSH_HIGHLIGHT_STYLES[commandseparator]
+                 else
+                   style=$ZSH_HIGHLIGHT_STYLES[default]
+                 fi
+                 ;;
+      esac
+    fi
+    # if a style_override was set (eg in _zsh_highlight_main_highlighter_check_path), use it
+    [[ -n $style_override ]] && style=$ZSH_HIGHLIGHT_STYLES[$style_override]
+    [[ $substr_color = 0 ]] && region_highlight+=("$start_pos $end_pos $style")
+    [[ -n ${(M)ZSH_HIGHLIGHT_TOKENS_FOLLOWED_BY_COMMANDS:#"$arg"} ]] && new_expression=true
+    start_pos=$end_pos
+  done
+}
+
+# Check if the argument is variable assignment
+_zsh_highlight_main_highlighter_check_assign()
+{
+    setopt localoptions extended_glob
+    [[ $arg == [[:alpha:]_][[:alnum:]_]#(|\[*\])=* ]]
+}
+
+# Check if the argument is a path.
+_zsh_highlight_main_highlighter_check_path()
+{
+  setopt localoptions nonomatch
+  local expanded_path; : ${expanded_path:=${(Q)~arg}}
+  [[ -z $expanded_path ]] && return 1
+  [[ -e $expanded_path ]] && return 0
+  # Search the path in CDPATH
+  local cdpath_dir
+  for cdpath_dir in $cdpath ; do
+    [[ -e "$cdpath_dir/$expanded_path" ]] && return 0
+  done
+  [[ ! -e ${expanded_path:h} ]] && return 1
+  if [[ ${BUFFER[1]} != "-" && ${#BUFFER} == $end_pos ]]; then
+    local -a tmp
+    # got a path prefix?
+    tmp=( ${expanded_path}*(N) )
+    (( $#tmp > 0 )) && style_override=path_prefix && return 0
+    # or maybe an approximate path?
+    tmp=( (#a1)${expanded_path}*(N) )
+    (( $#tmp > 0 )) && style_override=path_approx && return 0
+  fi
   return 1
 }
 
+# Highlight special chars inside double-quoted strings
+_zsh_highlight_main_highlighter_highlight_string()
+{
+  setopt localoptions noksharrays
+  local i j k style varflag
+  # Starting quote is at 1, so start parsing at offset 2 in the string.
+  for (( i = 2 ; i < end_pos - start_pos ; i += 1 )) ; do
+    (( j = i + start_pos - 1 ))
+    (( k = j + 1 ))
+    case "$arg[$i]" in
+      '$' ) style=$ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]
+            (( varflag = 1))
+            ;;
+      "\\") style=$ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]
+            for (( c = i + 1 ; c < end_pos - start_pos ; c += 1 )); do
+              [[ "$arg[$c]" != ([0-9,xX,a-f,A-F]) ]] && break
+            done
+            AA=$arg[$i+1,$c-1]
+            # Matching for HEX and OCT values like \0xA6, \xA6 or \012
+            if [[ "$AA" =~ "^(0*(x|X)[0-9,a-f,A-F]{1,2})" || "$AA" =~ "^(0[0-7]{1,3})" ]];then
+              (( k += $#MATCH ))
+              (( i += $#MATCH ))
+            else
+              (( k += 1 )) # Color following char too.
+              (( i += 1 )) # Skip parsing the escaped char.
+            fi
+              (( varflag = 0 )) # End of variable
+            ;;
+      ([^a-zA-Z0-9_]))
+            (( varflag = 0 )) # End of variable
+            continue
+            ;;
+      *) [[ $varflag -eq 0 ]] && continue ;;
+
+    esac
+    region_highlight+=("$j $k $style")
+  done
+}
+
+## pattern-highlighter.zsh
+# List of keyword and color pairs.
+typeset -gA ZSH_HIGHLIGHT_PATTERNS
+
+# Whether the pattern highlighter should be called or not.
+_zsh_highlight_pattern_highlighter_predicate()
+{
+  _zsh_highlight_buffer_modified
+}
+
+# Pattern syntax highlighting function.
+_zsh_highlight_pattern_highlighter()
+{
+  setopt localoptions extendedglob
+  local pattern
+  for pattern in ${(k)ZSH_HIGHLIGHT_PATTERNS}; do
+    _zsh_highlight_pattern_highlighter_loop "$BUFFER" "$pattern"
+  done
+}
+
+_zsh_highlight_pattern_highlighter_loop()
+{
+  # This does *not* do its job syntactically, sorry.
+  local buf="$1" pat="$2"
+  local -a match mbegin mend
+  if [[ "$buf" == (#b)(*)(${~pat})* ]]; then
+    region_highlight+=("$((mbegin[2] - 1)) $mend[2] $ZSH_HIGHLIGHT_PATTERNS[$pat]")
+    "$0" "$match[1]" "$pat"; return $?
+  fi
+}
+
+## root-highlighter.zsh
+# Define default styles.
+: ${ZSH_HIGHLIGHT_STYLES[root]:=standout}
+
+# Whether the root highlighter should be called or not.
+_zsh_highlight_root_highlighter_predicate()
+{
+  _zsh_highlight_buffer_modified
+}
+
+# root highlighting function.
+_zsh_highlight_root_highlighter()
+{
+  if [[ $(command id -u) -eq 0 ]] { region_highlight+=("0 $#BUFFER $ZSH_HIGHLIGHT_STYLES[root]") }
+}
+
+## syntax-highlighting.zsh
 # Reset scratch variables when commandline is done.
 _zsh_highlight_preexec_hook()
 {
